5df46dc40890ca1e0226fa1f049bd7479133eb83
maneeshd <maneeshd77@gmail.com>
2017-06-24 20:21:46 +0530

    Added timeit to Insertion Sort

--- fundamentals/InsertionSort.py
+++ fundamentals/InsertionSort.py
@@ -3,11 +3,13 @@
 @email: maneeshd77@gmail.com
 @date: 22/6/17
 
-Insertion Sort
+Insertion Sort -> O(n^2)
 """
+from random import randint
+from timeit import Timer, default_timer
 
 
-def insertion_sort():
+def insertion_sort(data):
     for i in range(1, len(data)):
         key = data[i]
         j = i - 1
@@ -17,8 +19,15 @@ def insertion_sort():
         data[j+1] = key
 
 
+def main():
+    start = default_timer()
+    data = [randint(1, 10) + i for i in range(100001)]
+    insertion_sort(data)
+    print("Sort Time = %f Seconds" % (default_timer() - start))
+
+
 if __name__ == '__main__':
-    data = [31, 41, 59, 26, 41, 58]
-    print("Before Sorting: %s" % data)
-    insertion_sort()
-    print("After Sorting: %s" % data)
+    print("Insertion Sort")
+    print("-" * len("Insertion Sort"))
+    t = Timer(main)
+    print("\nAverage sorting time for 100000 elements in 10 runs = %f Seconds" % (t.timeit(10)/10))
