9d692ebee2f8c899557d77216f9283f3a373da9c
maneeshd <maneeshd77@gmail.com>
2018-03-21 13:48:18 +0530

    Improvements to Stack

--- Data_Structures/Stack.py
+++ Data_Structures/Stack.py
@@ -5,6 +5,7 @@
 
 Stack
 """
+_STACK_SIZE_LIMIT = 50
 
 
 class Stack:
@@ -12,50 +13,119 @@ class Stack:
         self.top = -1
         self.stack = []
 
-    def push(self, data):
-        if len(self.stack) >= 10:
+    def __str__(self):
+        if self.is_empty():
+            return str([])
+        i = len(self.stack) - 1
+        ret_str = ""
+        while i > -1:
+            ret_str += "\t{}\n".format(self.stack[i])
+            i -= 1
+        return ret_str
+
+    def is_empty(self):
+        if self.top < 0 or len(self.stack) == 0:
+            return True
+        return False
+
+    def is_full(self):
+        if self.top >= _STACK_SIZE_LIMIT or len(self.stack) >= _STACK_SIZE_LIMIT:
+            return True
+        return False
+
+    @staticmethod
+    def type_checker(data: str):
+        try:
+            if data.isdigit():
+                return int(data)
+            elif data.lower() == "true":
+                return True
+            elif data.lower() == "false":
+                return False
+            elif data.lower() in ["none", "null", "nan"]:
+                return None
+            elif data.replace(".", "").isdigit():
+                return float(data)
+            else:
+                return data
+        except:
+            return data
+
+    def push(self, item):
+        if self.is_full():
             print("!!! Stack Overflow !!!\n")
             return
+        self.stack.append(Stack.type_checker(item))
         self.top += 1
-        self.stack.append(data)
-        print("%d pushed into stack..." % data)
+        print(item, "pushed into stack...")
 
     def pop(self):
-        if self.top < 0:
-            print("!!! Stack is Empty !!!\n")
+        if self.is_empty():
+            print("!!! Stack Underflow !!!\n")
             return
         data = self.stack[self.top]
         self.top -= 1
         return data
 
-    def display_stack(self):
-        i = len(self.stack) - 1
-        while i > -1:
-            print("\t\t%d" % self.stack[i])
-            i -= 1
-        print()
+    def peek(self):
+        if self.is_empty():
+            print("!!! Stack Underflow !!!")
+            return
+        return self.stack[self.top]
+
 
 if __name__ == '__main__':
-    print("Stack Operation")
-    print("-" * len("Stack Operation"))
-    stak = Stack()
+    print("Stack Operations")
+    print("-" * len("Stack Operations"))
+    my_stack = Stack()
+
     while True:
-        print("1. Push\n"
-              "2. Pop\n"
-              "3. Display\n"
-              "4. Exit\n")
-        choice = input("Enter your choice: ")
-        if choice == "1":
-            dat = int(input("Enter the number to push: "))
-            stak.push(dat)
-        elif choice == "2":
-            dat = stak.pop()
-            if dat:
-                print("Popped element is: %d\n" % dat)
-        elif choice == "3":
-            print("Stack: ")
-            stak.display_stack()
-        elif choice == "4":
-            exit(0)
-        else:
-            print("Enter a valid choice !!")
+        try:
+
+            print("[1] Push\n"
+                  "[2] Pop\n"
+                  "[3] Peek\n"
+                  "[4] Display\n"
+                  "[5] Exit\n")
+
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                dat = input("Enter the item to push: ")
+                my_stack.push(dat)
+                print()
+
+            elif choice == "2":
+                dat = my_stack.pop()
+                if dat:
+                    print("Popped item:", dat, "\n")
+                else:
+                    print()
+
+            elif choice == "3":
+                peek_item = my_stack.peek()
+                if peek_item:
+                    print("Top most item in stack:", peek_item, "\n")
+                else:
+                    print()
+
+            elif choice == "4":
+                print("Stack:")
+                print(my_stack, "\n")
+
+            elif choice == "5":
+                print("Exiting...\nThank You..\n")
+                exit(0)
+
+            else:
+                print("!!! Enter a valid choice !!!\n")
+
+        except ValueError:
+            print("!!! Enter a valid choice !!!\n")
+        except TypeError:
+            print("!!! Enter a valid choice !!!\n")
+        except KeyboardInterrupt:
+            print("Got Ctrl+C...\nExiting...\n")
+            exit(1)
+        except Exception as exp:
+            print(exp, "\n")
