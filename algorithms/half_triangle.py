f46bd2c077dbd241236a51c2fd9c29c670582722
Maneesh Divana <maneeshd77@gmail.com>
2020-02-07 20:45:17 -0600

    Addded run as 'main' check

--- algorithms/half_triangle.py
+++ algorithms/half_triangle.py
@@ -1,9 +1,11 @@
-# Prints Half Triangle
-for i in range(5):
-    for _ in range(i):
-        print("*", end=" ")
-    print("")
-for i in range(5, 0, -1):
-    for _ in range(i):
-        print("*", end=" ")
+if __name__ == "__main__":
+    # Prints Half Triangle
+    for i in range(5):
+        for _ in range(i):
+            print("*", end=" ")
+        print("")
+    for i in range(5, 0, -1):
+        for _ in range(i):
+            print("*", end=" ")
+        print("")
     print("")
